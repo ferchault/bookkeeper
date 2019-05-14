ceac4140b02843a229a91f3612b9b648bfafd016
Guido <guido@vonrudorff.de>
2019-05-14 16:40:21 +0200

    Split functions

---
+++ executor/common.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import sys, socket, tarfile, io, os, subprocess
+import redis, rq
+
+def get_connection():
+    connectionstring = open(os.path.expanduser('~/.redis-credentials')).readlines()[0].strip()
+    password, rest = connectionstring.split('@')
+    hostname, rest = rest.split(':')
+    port, db = rest.split('/')
+    return redis.Redis(host=hostname, port=int(port), db=1, password=password)
+
+def get_tarfile(dirname):
+    file_out = io.BytesIO()
+    tar = tarfile.open(mode="w:gz", fileobj=file_out)
+    tar.add(dirname, recursive=True, arcname='run')
+    tar.close()
+    return file_out.getvalue()
+
+def get_script(scriptname):
+    return '\n'.join(open(scriptname).readlines())
+
+def get_hostname():
+    return socket.gethostname()
+
+def run_in_memory(hostname, directory, script, targzfile):
+    file_in = io.BytesIO(targzfile)
+    tar = tarfile.open(mode="r:gz", fileobj=file_in)
+    tar.extractall('.')
+
+    with open('run/run.sh', 'w') as fh:
+        fh.write(script)
+    
+    subprocess.run('./run.sh > run.log', shell=True, cwd='run')
+
+    tarfile = get_tarfile('run')
+    return tarfile
--- executor/push.py
+++ executor/push.py
@@ -7,38 +7,7 @@ Usage: push.py long/short path/to/script.sh path/to/directory-list
 import sys, socket, tarfile, io, os, subprocess
 import redis, rq
 
-def get_connection():
-    connectionstring = open(os.path.expanduser('~/.redis-credentials')).readlines()[0].strip()
-    password, rest = connectionstring.split('@')
-    hostname, rest = rest.split(':')
-    port, db = rest.split('/')
-    return redis.Redis(host=hostname, port=int(port), db=1, password=password)
-
-def get_tarfile(dirname):
-    file_out = io.BytesIO()
-    tar = tarfile.open(mode="w:gz", fileobj=file_out)
-    tar.add(dirname, recursive=True, arcname='run')
-    tar.close()
-    return file_out.getvalue()
-
-def get_script(scriptname):
-    return '\n'.join(open(scriptname).readlines())
-
-def get_hostname():
-    return socket.gethostname()
-
-def run_in_memory(hostname, directory, script, targzfile):
-    file_in = io.BytesIO(targzfile)
-    tar = tarfile.open(mode="r:gz", fileobj=file_in)
-    tar.extractall('.')
-
-    with open('run/run.sh', 'w') as fh:
-        fh.write(script)
-    
-    subprocess.run('./run.sh > run.log', shell=True, cwd='run')
-
-    tarfile = get_tarfile('run')
-    return tarfile
+from common import *
 
 if __name__ == '__main__':
     # check args
