4fef52e870edcce7e54d10f2d9467e2e473273c5
Guido <guido@vonrudorff.de>
2019-05-15 16:09:33 +0200

    Adjusted path

--- executor/codes/g09.sh
+++ executor/codes/g09.sh
@@ -4,11 +4,17 @@ is_alchemy() {
 	scontrol show config | grep alchemy &> /dev/null
 }
 g09_alchemy() {
-	export GAUSS_EXEDIR="/home/admin/software/gaussian/g09_d.01/g09/"
+	export GAUSS_EXEDIR="/apps/gaussian/g09_d.01/g09/"
 	export PATH="$GAUSS_EXEDIR:$PATH"
 	export GAUSS_SCRDIR="$PWD"
 	$GAUSS_EXEDIR/g09 run.inp
 	$GAUSS_EXEDIR/formchk run.chk
+	rm run.chk
+	rm Gau-*.d2e
+	rm Gau-*.int
+	rm Gau-*.skr
+	rm Gau-*.rwf
+	rm Gau-*.inp
 }
 
 is_alchemy && g09_alchemy
