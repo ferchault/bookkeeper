bae82a36bdbdd0e6788c75d7252ffc803e9ca8d6
Guido <guido@vonrudorff.de>
2019-05-15 17:54:35 +0200

    Remove temporary files

--- executor/codes/orca.sh
+++ executor/codes/orca.sh
@@ -6,6 +6,7 @@ is_alchemy() {
 orca_alchemy() {
 	export PATH="/home/vonrudorff/orca/orca_4_0_1_2_linux_x86-64_openmpi202:$PATH"
 	/home/vonrudorff/orca/orca_4_0_1_2_linux_x86-64_openmpi202/orca run.inp
+	rm *.tmp *.gbw *aa.tmp.*
 }
 
 is_alchemy && orca_alchemy
