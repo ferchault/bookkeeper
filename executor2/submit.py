c0bb320d59e718d19092620845e11ee95afcffcc
Guido <guido@vonrudorff.de>
2020-02-25 08:32:42 +0100

    Bulk submission

--- executor2/submit.py
+++ executor2/submit.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 from redis import Redis
 from rq import Queue
-
-q = Queue(connection=Redis())
-
 from job_registry import base
 import sys
+
+q = Queue(connection=Redis())
 
 filename = sys.argv[1]
 commandstring = sys.argv[2]
 
-result = q.enqueue(base.work, args=(filename, commandstring), timeout=5*60, result_ttl=365*24*60*60)
-print (result.id)
+def enqueue(filename, commandstring):
+	result = q.enqueue(base.work, args=(filename, commandstring), timeout=5*60, result_ttl=365*24*60*60)
+	print (result.id)
+
+if commandstring == "-":
+	for line in sys.stdin:
+		line = line[:-1]
+		enqueue(filename, line)
+else:
+	enqueue(filename, commandstring)
+
