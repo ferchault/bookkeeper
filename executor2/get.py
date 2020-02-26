72b631e4c50e4d739f015e71df2f8b61727f0539
Guido <guido@vonrudorff.de>
2020-02-26 17:46:12 +0100

    In-place update of results

--- executor2/get.py
+++ executor2/get.py
@@ -3,6 +3,7 @@ from redis import Redis
 from rq import Queue
 from rq.job import Job
 import os
+import tqdm
 
 redis = Redis.from_url("redis://" + os.environ.get('EXECUTOR_CONSTR', "127.0.0.1:6379/0"))
 q = Queue(connection=redis)
@@ -10,27 +11,32 @@ q = Queue(connection=redis)
 from job_registry import base
 import sys
 
-jobid = sys.argv[1]
+current_resultfile = sys.argv[1]
 
 def read_result(jobid):
 	try:
 		job = Job.fetch(jobid, connection=redis)
 	except:
-		print ("JOB:", jobid, "ERRORNoSuchJob")
-		return
+		return "JOB: %s ERRORNoSuchJob" % jobid
 
 	if job.get_status() == "finished":
-		print (job.result)
+		result = job.result
 		job.delete()
+		return result
 	else:
-		print ("JOB:", jobid, "WARNING%s" % job.get_status())
+		return "JOB: %s WARNING%s" % (jobid, job.get_status())
 
-if jobid == "-":
-	for line in sys.stdin:
-		if line.startswith("JOB: "):
-			jobid = line.strip().split()[1]
-			read_result(jobid)
-		else:
-			print (line[:-1])
-else:
-	read_result(jobid)
+with open(current_resultfile) as fh:
+	lines = fh.readlines()
+
+results = []
+for line in tqdm.tqdm(lines):
+	line = line[:-1]
+	if line.startswith("JOB: "):
+		jobid = line.strip().split()[1]
+		results.append(read_result(jobid))
+	else:
+		results.append(line)
+
+with open(current_resultfile, "w") as fh:
+	fh.write('\n'.join(results) + "\n")
