8b178442c675e53dc1a2e33063f41766c33f5d1f
Guido <guido@vonrudorff.de>
2020-02-26 17:32:11 +0100

    Bulk insertion support

--- executor2/submit.py
+++ executor2/submit.py
@@ -4,20 +4,20 @@ from rq import Queue
 from job_registry import base
 import sys
 import os
+import tqdm
 
 q = Queue(connection=Redis.from_url("redis://" + os.environ.get('EXECUTOR_CONSTR', "127.0.0.1:6379/0")))
 
-filename = sys.argv[1]
-commandstring = sys.argv[2]
+taskname = sys.argv[1]
+arguments = sys.argv[2]
 
-def enqueue(filename, commandstring):
-	result = q.enqueue(base.work, args=(filename, commandstring), timeout=5*60, result_ttl=365*24*60*60)
+def enqueue(taskname, commandstring):
+	result = q.enqueue(base.work, args=(taskname, commandstring), timeout=5*60, result_ttl=365*24*60*60, description="auto")
 	print ("JOB:", result.id)
 
-if commandstring == "-":
-	for line in sys.stdin:
+
+with open(arguments) as fh:
+	lines = fh.readlines()
+	for line in tqdm.tqdm(lines):
 		line = line[:-1]
 		enqueue(filename, line)
-else:
-	enqueue(filename, commandstring)
-
