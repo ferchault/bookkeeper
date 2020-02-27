f20a8e664e6392575318495c85616f651d6922fa
von Rudorff, Guido Falk <guido@vonrudorff.de>
2020-02-27 18:06:41 +0100

    Current redis throughput stats

---
+++ executor2/stats.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from redis import Redis
+import glob
+import os
+import json
+import time
+
+redis = Redis.from_url("redis://" + os.environ.get('EXECUTOR_CONSTR', "127.0.0.1:6379/0"))
+
+def get_throughput(source):
+	items = redis.llen("stats:%s:duration" % source)
+	remaining = int(redis.pttl("stats:" + source))
+	elapsed = 60000 - remaining
+	return float(elapsed)/1000, items
+
+res = []
+for source in glob.glob("job_registry/*.py"):
+	source = source.split("/")[1][:-3]
+	if source.startswith("__"):
+		continue
+	
+	if not redis.exists("stats:" + source):
+		continue
+
+	elapsed, items = get_throughput(source)
+	if elapsed < 10:
+		time.sleep(10)
+		elapsed, items = get_throughput(source)
+
+	failed = redis.get("stats:%s:failed" % source)
+	if failed is None:
+		failed = 0
+	res.append({'source': source, 'throughput': items / elapsed, 'failed': failed, 'when': time.time()})
+	
+print (json.dumps(res))	
+
