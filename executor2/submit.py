447294ff0f67c107a85c31207a30709035ba2556
von Rudorff, Guido Falk <guido@vonrudorff.de>
2020-02-27 13:25:30 +0100

    File storage

--- executor2/submit.py
+++ executor2/submit.py
@@ -10,14 +10,18 @@ q = Queue(connection=Redis.from_url("redis://" + os.environ.get('EXECUTOR_CONSTR
 
 taskname = sys.argv[1]
 arguments = sys.argv[2]
+resultfile = sys.argv[3]
 
 def enqueue(taskname, commandstring):
 	result = q.enqueue(base.work, args=(taskname, commandstring), timeout=5*60, result_ttl=365*24*60*60, description="auto")
-	print ("JOB:", result.id)
+	return "JOB: %s" % result.id
 
 
+results = []
 with open(arguments) as fh:
 	lines = fh.readlines()
 	for line in tqdm.tqdm(lines):
 		line = line[:-1]
-		enqueue(taskname, line)
+		results.append(enqueue(taskname, line))
+with open(resultfile, 'w') as fh:
+	fh.write('\n'.join(results))
