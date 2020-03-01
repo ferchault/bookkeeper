d3a4c9a33e0304b71fa0d88b1aa9edc412474fca
Guido <guido@vonrudorff.de>
2020-03-01 12:21:05 +0100

    Activate autoscaler

--- executor2/autoscaler.py
+++ executor2/autoscaler.py
@@ -4,6 +4,7 @@ import getpass
 from redis import Redis
 import os
 import subprocess
+import time
 
 constr = os.environ.get('EXECUTOR_CONSTR', "127.0.0.1:6379/0")
 
@@ -13,6 +14,10 @@ parts[-1] = "0"
 constr = '/'.join(parts)
 
 redis = Redis.from_url("redis://" + constr)
+
+# check operational
+if redis.get("meta:operational").decode("ascii") != "yes":
+	sys.exit(0)
 
 hostname = base.get_hostname()
 username = getpass.getuser()
@@ -27,6 +32,18 @@ with open(tmpfile) as fh:
 running = len([_ for _ in lines if "RUNNING" in _])
 pending = len(lines) - running
 
+# submit additional jobs
+max_pending = 50
+free_slots = max(0, max_pending - pending)
+keyname = "meta:submitted:%d" % (int(time.time()) // 3600)
+added = 0
+for i in range(free_slots):
+	has_work = redis.rpoplpush("meta:capacity", keyname)
+	if has_work is None:
+		break
+	subprocess.run(['sbatch', 'runners/%s.job' % hostname])
+	added += 1
+
 keyname = "%s-%s" % (hostname, username)
-redis.hset("meta:queued", keyname, pending)
+redis.hset("meta:queued", keyname, pending + added)
 redis.hset("meta:running", keyname, running)
