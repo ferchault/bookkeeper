81fd2dc7ae3d57b360e5bc541ca21c21adb78f30
Guido Falk von Rudorff <guidofalk.vonrudorff@unibas.ch>
2020-03-18 13:57:15 +0100

    Support for qos balancing

--- executor2/autoscaler.py
+++ executor2/autoscaler.py
@@ -23,27 +23,43 @@ hostname = base.get_hostname()
 username = getpass.getuser()
 
 tmpfile = "/tmp/%s.autoscaler-tmp" % username
-with open(tmpfile, "w") as fh:
-	subprocess.run(["squeue", "-u", username, "-r", "-h", "-n", "executor", "-O", "state"], stdout=fh, stderr=fh)
 
-with open(tmpfile) as fh:
-	lines = fh.readlines()
+if hostname == "scicore":
+	qoss = ['30min', '6hours']
+	times = ['00:30:00', '06:00:00']
+else:
+	qoss = ['noqos']
+	times = ['notime']
 
-running = len([_ for _ in lines if "RUNNING" in _])
-pending = len(lines) - running
+for qos, stime in zip(qoss, times):
+	args = ["squeue", "-u", username, "-r", "-h", "-n", "executor", "-O", "state"]
+	if qos != 'noqos':
+		args += ["-q", qos]
+	with open(tmpfile, "w") as fh:
+		subprocess.run(args, stdout=fh, stderr=fh)
 
-# submit additional jobs
-max_pending = 50
-free_slots = max(0, max_pending - pending)
-keyname = "meta:submitted:%d" % (int(time.time()) // 3600)
-added = 0
-for i in range(free_slots):
-	has_work = redis.rpoplpush("meta:capacity", keyname)
-	if has_work is None:
-		break
-	subprocess.run(["sbatch", "runners/%s.job" % hostname])
-	added += 1
+	with open(tmpfile) as fh:
+		lines = fh.readlines()
 
-keyname = "%s-%s" % (hostname, username)
-redis.hset("meta:queued", keyname, pending + added)
-redis.hset("meta:running", keyname, running)
+	running = len([_ for _ in lines if "RUNNING" in _])
+	pending = len(lines) - running
+
+	# submit additional jobs
+	max_pending = 50
+	free_slots = max(0, max_pending - pending)
+	keyname = "meta:submitted:%d" % (int(time.time()) // 3600)
+	added = 0
+	for i in range(free_slots):
+		has_work = redis.rpoplpush("meta:capacity", keyname)
+		if has_work is None:
+			break
+		if qos == 'noqos':
+			args = ["sbatch", "runners/%s.job" % hostname]
+		else:
+			args = ["sbatch", "--qos", qos, "--time", stime, "runners/%s.job" % hostname]
+		subprocess.run(args)
+		added += 1
+
+	keyname = "%s-%s-%s" % (hostname, username, qos)
+	redis.hset("meta:queued", keyname, pending + added)
+	redis.hset("meta:running", keyname, running)
