bce8cde8183dcc85c810e2caca16229aee3cd4bc
Guido <guido@vonrudorff.de>
2020-03-27 22:10:21 +0100

    QML distributed predictions

---
+++ executor2/job_registry/qmlpredict.py
@@ -0,0 +1,63 @@
+import qml
+import numpy as np
+import os
+from redis import Redis
+import json
+import gzip
+
+#redis = Redis.from_url("redis://" + os.environ.get("EXECUTOR_CONSTR", "127.0.0.1:6379/0"))
+
+class MockXYZ(object):
+	def __init__(self, lines):
+		self._lines = lines
+
+	def readlines(self):
+		return self._lines
+
+class Task():
+	def _save(self, label, variable):
+		redis.set(f"qmlpredict-cache:{label}", variable.tostring('C'))
+		shape = ':'.join([str(_) for _ in variable.shape])
+		redis.set(f"qmlpredict-cache:{label}:shape", shape)
+
+	def _upload(self):
+		cutoff = 9000
+		basename = '/mnt/c/Users/guido/data/tmp-enrico/'
+		included = open(basename + 'index_nonegative.dat').readlines()[:cutoff]
+		lines = []
+		for geo in included:
+			geo = geo.strip().rjust(5, '0')
+			filename = f'{basename}random/random-{geo}.xyz'
+			lines += [_.strip() for _ in open(filename).readlines()]
+		
+		redis.set("qml-structures", gzip.compress(('\n'.join(lines)).encode('ascii')))
+		redis.set("qml-alphas", gzip.compress(open(f'{basename}alpha.dat').read().encode('ascii')))
+		
+	def __init__(self):
+		#self._upload()
+
+		lines = gzip.decompress(redis.get("qml-structures")).decode('ascii').split("\n")
+		q = gzip.decompress(redis.get("qml-alphas")).decode('ascii').strip().split("\n")
+		alphas = np.array([float(_) for _ in q])
+		
+		reps = []
+		Qs = []
+		for geoidx in range(len(alphas)):
+			c = qml.Compound(xyz=MockXYZ(lines[geoidx*33:(geoidx+1)*33]))
+			reps.append(qml.representations.generate_fchl_acsf(c.nuclear_charges, c.coordinates, gradients=False, pad=31, elements=[1,6,8]))
+			Qs.append(c.nuclear_charges)
+
+		self._reps = np.array(reps)
+		self._Qs = np.array(Qs)
+		self._alphas = alphas
+		
+	def run(self, commandstring):
+		content = json.loads(commandstring)
+		xyz = MockXYZ(content['neutralgeometry'].split("\n"))
+		c = qml.Compound(xyz=xyz)
+		rep = qml.representations.generate_fchl_acsf(c.nuclear_charges, c.coordinates, gradients=False, pad=31, elements=[1,6,8])
+		
+		K = qml.kernels.get_local_kernel(self._reps, np.array([rep]), self._Qs, [c.nuclear_charges], 65.536)
+		Yss = np.dot(K, self._alphas)
+
+		return str(Yss[0])
