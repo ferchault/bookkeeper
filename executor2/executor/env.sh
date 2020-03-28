ff71b56dac221e53b97e67c33022be094ca7e82e
Guido <guido@vonrudorff.de>
2020-03-28 19:30:51 +0100

    qml env

--- executor2/executor/env.sh
+++ executor2/executor/env.sh
@@ -4,3 +4,5 @@ conda install numpy scipy pandas
 conda install -c conda-forge rdkit
 conda install openbabel -c conda-forge
 pip install quadpy lz4 redis
+pip install git+https://github.com/qmlcode/qml@18f4e9b4fec38b71c9246fb2735a4821229e37b8 --user -U
+# patch "/home/guido/.local/lib/python3.8/site-packages/qml/models/kernelridge.py" to remove ase dependency
