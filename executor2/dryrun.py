96d161a27d6fe75884f8481f3ee8ca2461267bb2
Guido <guido@vonrudorff.de>
2020-03-28 11:45:17 +0100

    Expose redis in dryruns

--- executor2/dryrun.py
+++ executor2/dryrun.py
@@ -5,12 +5,14 @@ import os
 import time
 import traceback
 import importlib
+from redis import Redis
 import signal
 
+redis = Redis.from_url("redis://" + os.environ.get("EXECUTOR_CONSTR", "127.0.0.1:6379/0"))
 filename = sys.argv[1]
 
 mod = importlib.import_module("job_registry.%s" % filename)
-task = mod.Task()
+task = mod.Task(redis)
 
 for line in sys.stdin:
 	commandstring = line[:-1]
