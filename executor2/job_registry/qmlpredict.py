361cb193c1bc03024d4fb307bd35b1048943d8f9
Guido <guido@vonrudorff.de>
2020-05-04 23:08:57 +0200

    Support for 3 predictions at once

--- executor2/job_registry/qmlpredict.py
+++ executor2/job_registry/qmlpredict.py
@@ -18,27 +18,31 @@ class Task():
 	def _upload(self):
 		cutoff = 9000
 		basename = '/mnt/c/Users/guido/data/tmp-enrico/'
-		#included = open(basename + 'index_nonegative.dat').readlines()[:cutoff]
-		#lines = []
-		#for geo in included:
-		#	geo = geo.strip().rjust(5, '0')
-		#	filename = f'{basename}random/random-{geo}.xyz'
-		#	lines += [_.strip() for _ in open(filename).readlines()]
+		included = open(basename + 'production/index_nonegative_new.dat').readlines()[:cutoff]
+		lines = []
+		import tqdm
+		for geo in tqdm.tqdm(included):
+			geo = geo.strip().rjust(5, '0')
+			filename = f'{basename}random/random-{geo}.xyz'
+			lines += [_.strip() for _ in open(filename).readlines()]
 		
-		#self.connection.set("qml-structures", gzip.compress(('\n'.join(lines)).encode('ascii')))
-		self.connection.set("qml-alphas1", gzip.compress(open(f'{basename}alpha-o1.dat').read().encode('ascii')))
-		self.connection.set("qml-alphas2", gzip.compress(open(f'{basename}alpha-o2.dat').read().encode('ascii')))
+		self.connection.set("qml-structures", gzip.compress(('\n'.join(lines)).encode('ascii')))
+		self.connection.set("qml-alphas1", gzip.compress(open(f'{basename}production/alphaS1_65.536_7.dat').read().encode('ascii')))
+		self.connection.set("qml-alphas2", gzip.compress(open(f'{basename}production/alphaS2_65.536_7.dat').read().encode('ascii')))
+		self.connection.set("qml-alphas3", gzip.compress(open(f'{basename}production/alphaS3_65.536_7.dat').read().encode('ascii')))
 		
 	def __init__(self, connection):
 		self.connection = connection
 		#self._upload()
-		#return
+		#return "uploaded"
 
 		lines = gzip.decompress(self.connection.get("qml-structures")).decode('ascii').split("\n")
 		q = gzip.decompress(self.connection.get("qml-alphas1")).decode('ascii').strip().split("\n")
 		alphas1 = np.array([float(_) for _ in q])
 		q = gzip.decompress(self.connection.get("qml-alphas2")).decode('ascii').strip().split("\n")
 		alphas2 = np.array([float(_) for _ in q])
+		q = gzip.decompress(self.connection.get("qml-alphas3")).decode('ascii').strip().split("\n")
+		alphas3 = np.array([float(_) for _ in q])
 		
 		reps = []
 		Qs = []
@@ -51,6 +55,7 @@ class Task():
 		self._Qs = np.array(Qs)
 		self._alphas1 = alphas1
 		self._alphas2 = alphas2
+		self._alphas3 = alphas3
 		
 	def run(self, commandstring):
 		if "ERROR" in commandstring:
@@ -60,8 +65,9 @@ class Task():
 		c = qml.Compound(xyz=xyz)
 		rep = qml.representations.generate_fchl_acsf(c.nuclear_charges, c.coordinates, gradients=False, pad=31, elements=[1,6,8])
 		
-		K = qml.kernels.get_local_kernel(self._reps, np.array([rep]), self._Qs, [c.nuclear_charges], 0.128)
+		K = qml.kernels.get_local_kernel(self._reps, np.array([rep]), self._Qs, [c.nuclear_charges], 65.536)
 		preds1 = np.dot(K, self._alphas1)[0]
 		preds2 = np.dot(K, self._alphas2)[0]
+		preds3 = np.dot(K, self._alphas3)[0]
 
-		return str(preds1) + "," + str(preds2)
+		return str(preds1) + "," + str(preds2) + "," + str(preds3)
