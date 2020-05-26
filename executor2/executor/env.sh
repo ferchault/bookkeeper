6bcd7237b24ee5869e03c96895c6c53e2cb3cecd
Guido <guido@vonrudorff.de>
2020-05-26 15:26:14 +0200

    Added new dependencies

--- executor2/executor/env.sh
+++ executor2/executor/env.sh
@@ -3,6 +3,7 @@ conda create -n redis-worker python=3.8
 conda install numpy scipy pandas
 conda install -c conda-forge rdkit
 conda install openbabel -c conda-forge
-pip install quadpy lz4 redis
+pip install quadpy lz4 redis networkx
 pip install git+https://github.com/qmlcode/qml@18f4e9b4fec38b71c9246fb2735a4821229e37b8 --user -U
 # patch "/home/guido/.local/lib/python3.8/site-packages/qml/models/kernelridge.py" to remove ase dependency
+# deploy https://raw.githubusercontent.com/ferchault/xyz2mol/master/xyz2mol.py
