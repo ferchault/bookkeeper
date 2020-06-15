71341f0b67b4aede7cd1038aaf049ae8a4e87de4
Guido <guido@vonrudorff.de>
2020-06-15 11:53:59 +0200

    Added numba dependency

--- executor2/executor/env.sh
+++ executor2/executor/env.sh
@@ -2,6 +2,7 @@
 conda create -n redis-worker python=3.8
 conda install numpy scipy pandas
 conda install -c conda-forge rdkit
+conda install numba
 conda install openbabel -c conda-forge
 pip install quadpy redis networkx
 pip install git+https://github.com/qmlcode/qml@18f4e9b4fec38b71c9246fb2735a4821229e37b8 --user -U
